c1ad7b4b0bd1af8654f5cecf5c2522f3d42a0395
François Rousselet <francois.rousselet@rslt.fr>
2022-01-25 13:53:20 +0100

    WIP: Add auth

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/frousselet/freebox-sdk-go
+
+go 1.17
---
+++ test/authentication.go
@@ -0,0 +1,7 @@
+package test
+
+import (
+	"github.com/frousselet/freebox-sdk-go/freebox"
+)
+
+freebox.RequestAuthorization()
